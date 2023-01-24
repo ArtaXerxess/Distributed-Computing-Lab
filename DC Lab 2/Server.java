2bedc97556afd30382f9d2c8df6f62c486491d8b
Harshvardhan Singh <74452252+ArtaXerxess@users.noreply.github.com>
2023-01-24 12:58:55 +0530

    Add files via upload

---
+++ DC Lab 2/Server.java
@@ -0,0 +1,35 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server implements Hello {
+
+    public Server() {}
+
+    public String sayHello() {
+        return "Hello, world!";
+    }
+
+    public int add(int a , int b)
+    {
+        return a+b;
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            Server obj = new Server();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ DC Lab 2/writeup_and_code.md
@@ -0,0 +1,13 @@
+# lab 2
+
+
+https://docs.oracle.com/javase/8/docs/technotes/guides/rmi/hello/hello-world.html#startRegistry
+
+
+
+writeup and code:
+
+https://www.javatpoint.com/RMI
+
+
+and gfg
